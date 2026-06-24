7fb0c0570b1371deefb542a4917c7f6a55f4bd90
Flag <frankyboy10@hotmail.ca>
2026-06-24 09:08:07 -0400

    started backend organization

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Home</h1>
+  <h1>Muscle Maker</h1>
+
+  <!-- Div to place filters -->
+  <div></div>
+
+  <!-- Div to place generation buttons -->
+  <div></div>
+
+  <!-- Div to display generated program -->
+  <div></div> 
+
 </template>
 
 <script setup>
