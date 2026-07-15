d460e74e1c205b1b2666a6bc7fa8d506684b1222
Flag <frankyboy10@hotmail.ca>
2026-07-15 16:22:15 -0400

    started working on filters frontend

---
+++ frontend/src/components/filtersDisplay.vue
@@ -0,0 +1,78 @@
+<template>
+    <label for="durationFilter">Program's duration: </label>
+    <select name="durationFilter" id="durationFilter">
+        <option value="30">30 minutes</option>
+        <option value="45">45 minutes</option>
+        <option value="60">1 hour</option>
+        <option value="75">1 hour 15 minutes</option>
+        <option value="90">1 hour 30 minutes</option>
+        <option value="105">1 hour 45 minutes</option>
+        <option value="120">2 hours</option>
+    </select>
+
+    <label for="levelFilter">Experience : </label>
+    <select name="levelFilter" id="levelFilter">
+        <option value="1">Just started</option>
+        <option value="2">Beginner</option>
+        <option value="3">Intermediate</option>
+        <option value="4">Advanced</option>
+    </select>
+
+  <h2>Equipment Available</h2>
+
+  <label>
+    <input
+      type="checkbox"
+      value="Barbell"
+      v-model="equipment"
+    />
+    Barbell
+  </label>
+
+  <br>
+
+  <label>
+    <input
+      type="checkbox"
+      value="Dumbbells"
+      v-model="equipment"
+    />
+    Dumbbells
+  </label>
+
+  <br>
+
+  <label>
+    <input
+      type="checkbox"
+      value="Machines"
+      v-model="equipment"
+    />
+    Machines
+  </label>
+
+  <br>
+
+  <label>
+    <input
+      type="checkbox"
+      value="Bodyweight"
+      v-model="equipment"
+    />
+    Bodyweight
+  </label>
+
+  <p>Selected equipment:</p>
+  <pre>{{ equipment }}</pre>
+
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const equipment = ref([])
+
+</script>
+
+<style scoped> 
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <h1>Muscle Maker</h1>
 
   <!-- Div to place filters -->
-  <div></div>
+  <div>
+    <filtersDisplay />
+  </div>
 
   <!-- Div to place generation buttons -->
   <div>
@@ -21,6 +23,7 @@ import { ref } from "vue"
 import { generate_program } from "@/services/api"
 import generateButton from "@/components/generateButton.vue"
 import programDisplay from "@/components/programDisplay.vue"
+import filtersDisplay from "@/components/filtersDisplay.vue"
 
 const generatedProgram = ref(null)
 
@@ -32,18 +35,5 @@ async function handleGenerateProgram() {
 
 <style scoped>
 
-.button {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-    border-radius: 4px;
-}
 
 </style>
