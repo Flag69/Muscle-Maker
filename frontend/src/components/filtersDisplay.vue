3543b38bed710c44c48483d0b7038c2123ea37b2
Flag <frankyboy10@hotmail.ca>
2026-07-16 17:10:24 -0400

    working on filters functions

--- frontend/src/components/filtersDisplay.vue
+++ frontend/src/components/filtersDisplay.vue
@@ -2,45 +2,66 @@
 
     <h4>Basic parameters</h4>
 
-    <label for="durationFilter">Program's duration: </label>
-    <select name="durationFilter" id="durationFilter">
-        <option value="30">30 minutes</option>
-        <option value="45">45 minutes</option>
-        <option value="60">1 hour</option>
-        <option value="75">1 hour 15 minutes</option>
-        <option value="90">1 hour 30 minutes</option>
-        <option value="105">1 hour 45 minutes</option>
-        <option value="120">2 hours</option>
-    </select>
+    <div class="filter-container">
+        <div class="filter-container-row">
+            <label for="durationFilter">Program's duration: </label>
+            <select name="durationFilter" id="durationFilter">
+                <option value="30">30 minutes</option>
+                <option value="45">45 minutes</option>
+                <option value="60">1 hour</option>
+                <option value="75">1 hour 15 minutes</option>
+                <option value="90">1 hour 30 minutes</option>
+                <option value="105">1 hour 45 minutes</option>
+                <option value="120">2 hours</option>
+            </select>
+        </div>
 
-    <label for="levelFilter">Experience : </label>
-    <select name="levelFilter" id="levelFilter">
-        <option value="1">Just started</option>
-        <option value="2">Beginner</option>
-        <option value="3">Intermediate</option>
-        <option value="4">Advanced</option>
-    </select>
+        <div class="filter-container-row">
+            <label for="levelFilter">Experience: </label>
+            <select name="levelFilter" id="levelFilter">
+                <option value="1">Just started</option>
+                <option value="2">Beginner</option>
+                <option value="3">Intermediate</option>
+                <option value="4">Advanced</option>
+            </select>
+        </div>
+    </div>
 
     <h4>Targeted muscle groups</h4>
 
-        <div class="checkbox-group">
-            <label v-for="item in musclegroupsOptions" :key="item" class="checkbox-label">
-                <input type="checkbox" :value="item" v-model="musclegroups" />
+    <div class="checkbox-group">
+        <label v-for="item in musclegroupsOptions" :key="item" class="checkbox-label">
+            <input type="checkbox" :value="item" v-model="musclegroups" @change="toggleMuscleGroup(item)" />
 
-                {{ item }}
+            {{ item }}
 
-            </label>
-        </div>
+        </label>
+    </div>
+
+    <h4>Available equipment</h4>
+
+    <div class="checkbox-group">
+        <label v-for="item in availableEquipmentOptions" :key="item" class="checkbox-label">
+            <input type="checkbox" :value="item" v-model="availableEquipment" @change="toggleAvailableEquipment(item)" />
+
+            {{ item }}
+
+        </label>
+    </div>
 
         <p>Selected muscle groups:</p>
         <pre>{{ musclegroups }}</pre>
+
+        <p>Selected available equipment:</p>
+        <pre>{{ availableEquipment }}</pre>
 
 </template>
 
 <script setup>
 import { ref } from "vue"
 
-const musclegroups = ref([])
+const musclegroups = ref(["Everything"])
+const availableEquipment = ref(["Everything"])
 
 const musclegroupsOptions = [
     "Everything",
@@ -51,6 +72,43 @@ const musclegroupsOptions = [
     "Legs",
     "Core"
 ]
+
+const availableEquipmentOptions = [
+    "Everything",
+    "Nothing (bodyweight only)",
+    "Dumbbells",
+    "Barbells",
+    "Kettlebells",
+    "Cable machines",
+    "Pulley machines",
+    "Weights machines",
+    "Platforms",
+    "Squat racks",
+    "Benches",
+    "Resistance bands",
+    "Pull-up bars",
+    "Medicine balls",
+]
+
+function toggleMuscleGroup(item) {
+    if (item === 'Everything') {
+        musclegroups.value = ['Everything']
+    } else {
+        musclegroups.value = musclegroups.value.filter(
+            group => group !== 'Everything'
+        )
+    }
+}
+
+function toggleAvailableEquipment(item) {
+    if (item === 'Everything') {
+        availableEquipment.value = ['Everything']
+    } else {
+        availableEquipment.value = availableEquipment.value.filter(
+            equip => equip !== 'Everything'
+        )
+    }
+}
 
 </script>
 
@@ -66,6 +124,19 @@ const musclegroupsOptions = [
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(120px, 0.5fr));
     gap: 0.3rem 0.5rem;
+}
+
+.filter-container {
+    display: flex;
+    gap: 1rem 1rem;
+    flex-wrap: wrap;
+}
+
+.filter-container-row {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+    flex-wrap: nowrap;
 }
 
 </style>
