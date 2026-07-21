51e602182891487c7c2c06a415fd564085564da4
Flag <frankyboy10@hotmail.ca>
2026-07-21 17:33:13 -0400

    workout algo. progress

--- backend/app/database/db_functions.py
+++ backend/app/database/db_functions.py
@@ -1,5 +1,7 @@
 # Contains functions to query the database
 
+import random
+
 from app.database.models import Exercise
 
 def create_exercise(db_session, name):
@@ -12,17 +14,33 @@ def create_exercise(db_session, name):
 def get_all_exercises(db_session):
     return db_session.query(Exercise).all()
 
-def get_random_exercises_main(db_session, count=1, experienceLevel = 4, difficultyLevel = 10, targetedMuscleGroups = ["Everything"], availableEquipments = ["Everything"]):
+# Get random exercises based on different criterias
+def get_random_exercises(db_session, count=1, experienceLevel = 4, difficultyLevel = 10, targetedMuscleGroups = None, targetedMuscleType = "main", availableEquipments = None):
     query = db_session.query(Exercise)
 
     if experienceLevel:
         query = query.filter(Exercise.experienceLevel <= experienceLevel)
     if difficultyLevel:
         query = query.filter(Exercise.difficultyLevel <= difficultyLevel)
-    if targetedMuscleGroups and "Everything" not in targetedMuscleGroups:
-        query = query.filter(Exercise.primaryMuscles is in targetedMuscleGroups)
-    if availableEquipments and "Everything" not in availableEquipments:
-        query = query.filter(Exercise.requiredEquipments.contains(availableEquipments))
 
-    return query.order_by(func.random()).limit(count).all()
-    
+    exercises = query.all()
+
+    filtered_exercises = []
+    available_equipment_set = (set(availableEquipments) if availableEquipments else None)
+    for exercise in exercises:
+
+        match targetedMuscleType:
+            case "main":
+                if targetedMuscleGroups and exercise.primaryMuscles not in targetedMuscleGroups:
+                    continue
+            case "secondary":
+                if targetedMuscleGroups and exercise.secondaryMuscles not in targetedMuscleGroups:
+                    continue
+        if available_equipment_set:
+            if not set(exercise.requiredEquipments).issubset(available_equipment_set):
+                continue
+        filtered_exercises.append(exercise)
+
+    selected_exercises = random.sample(filtered_exercises, min(count, len(filtered_exercises)))
+
+    return selected_exercises
--- backend/app/services/generator.py
+++ backend/app/services/generator.py
@@ -1,14 +1,30 @@
 # Contains functions for workout generation and manipulation
 
-from app.database.db_functions import get_all_exercises
+from app.database.db_functions import get_all_exercises, get_random_exercises
 
 def generate_workout(db_session, programDuration = 60, programDifficulty = 3, programGoal = "No particular goal", targetedMuscleGroups = ["Everything"], availableEquipments = ["Everything"]):
     workout = []
-    activation = ...
     
-    exercises = get_all_exercises(db_session)
 
-    for exercise in exercises:
-        workout.append(exercise.name)
+    match programDuration:
+        case 45, 60:
+            activation_p = get_random_exercises(db_session, count=2, experienceLevel = programDifficulty, difficultyLevel = 5, targetedMuscleGroups = targetedMuscleGroups, targetedMuscleType = "main", availableEquipments = availableEquipments)
+            main_p = get_random_exercises(db_session, count=2, experienceLevel = programDifficulty, difficultyLevel = 7, targetedMuscleGroups = targetedMuscleGroups, targetedMuscleType = "secondary", availableEquipments = availableEquipments)
+            finisher_p = get_random_exercises(db_session, count=1, experienceLevel = programDifficulty, difficultyLevel = 5, targetedMuscleGroups = targetedMuscleGroups, targetedMuscleType = "main", availableEquipments = availableEquipments)
+        case 75, 90:
+            pass
+        case 105, 120:
+            pass
+    
+    for exercise in activation_p:
+        exo = {
+            "name": exercise.name,
+            "description": exercise.description,
+            "equipment": exercise.requiredEquipments,
+            "series": 3,
+            "repetitions": round((10 - exercise.difficultyLevel) * 1.5),
+            "rest": 60
+        }
+        workout.append(exo)
 
     return workout
